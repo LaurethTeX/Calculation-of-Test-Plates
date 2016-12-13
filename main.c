3969dad063556569647cf8f5c9a31914cfa0bb8b
Andrea Hidalgo <andrea@inaoep.mx>
2016-12-13 13:39:52 -0600

    Start programming !

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Declaracion de variables
+
+    //Indice de refraccion
+
+    //Distancia a la fuente
+        //Proceso
+
+    //Radio de curvatura de la lente
+
+    //Concava o convexa
+
+    //Diametro de la lente
+
+    //Test Plate Thickness
+
+
+    //Hacer primer calculo
+
+    //Iteraciones hasta tener thickness requerido
+
+    //Calcular back focal lenght
+
+
+    printf("Hello world!\n");
+    return 0;
+}
